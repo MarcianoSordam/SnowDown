3d29d0a4d50a2de1933a596d8389f24adddea87f
Menno Engels <menno.engels@live.nl>
2020-11-09 00:04:22 +0100

    Enemy now shoots carrots

---
+++ Assets/Carrot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrot : MonoBehaviour
+{
+    public double Damage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.SendMessage("TakeDamage",Damage);
+        Destroy(gameObject);
+    }
+}
