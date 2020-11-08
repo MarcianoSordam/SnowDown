f25f05126a585accc442134be1225f28fc00a016
Marciano <marcianosordam@gmail.com>
2020-11-08 23:32:20 +0100

    EnemyShoow (shoot) v0.2

--- Assets/Scripts/EnemyShoow.cs
+++ Assets/Scripts/EnemyShoow.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class EnemyShoow : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float delay = 2;
+    bool isShooting;
     void Update()
     {
+        if (true && !isShooting)//raycast hit player or wall & if not shooting
+        {
+            StartCoroutine(Shoot());
+            isShooting = true;
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        //shoot bullet
         
+        yield return new WaitForSeconds(delay);
+        isShooting = false;
+
     }
 }
