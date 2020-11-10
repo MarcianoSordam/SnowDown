66d4fb578574aabe634cd94dc08569c288f30da2
Menno Engels <menno.engels@live.nl>
2020-11-10 18:40:33 +0100

    snowball deals damage and self destructs after x seconds

--- Assets/GrowingSnowball.cs
+++ Assets/GrowingSnowball.cs
@@ -7,11 +7,13 @@ public class GrowingSnowball : MonoBehaviour
     [SerializeField] float rollSpeed = 10f;
     [SerializeField] float growSpeed = 1f;
     [SerializeField] float maxSize = 5f;
+    [SerializeField] float damage = 1;
+    [SerializeField] float destroyTime = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Destroy(gameObject, destroyTime);
     }
 
     void FixedUpdate()
@@ -22,9 +24,21 @@ public class GrowingSnowball : MonoBehaviour
         // this method doesn't work
         //rb.velocity = transform.forward * rollSpeed;
 
-        // increase size of the ball until maximum scale is reached
+        // increase size and damage of the ball until maximum size is reached
         if(maxSize > transform.localScale.x) {
             transform.localScale += Vector3.one * growSpeed * Time.deltaTime ;
+            damage++;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.SendMessage("TakeDamage",damage);
+
+        
+        if (other.gameObject.layer != 13) {
+            //todo snowball explode animation
+            Destroy(gameObject);
         }
     }
 }
